354ddb20721862fd09584127b40c1667ef9bb91f
Matthew McNaughton <mcnaughton.m@proton.me>
2024-12-11 15:35:31 -0500

    bug fix

--- mapit.py
+++ mapit.py
@@ -7,4 +7,4 @@ if len(sys.argv) > 1:
 else:
     address = pyperclip.paste()
 
-webbrowser.open('https://www.google.com/maps/place' + address)
+webbrowser.open('https://www.google.com/maps/place/' + address)
